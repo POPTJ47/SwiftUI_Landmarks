37cfee4dcae76e48b66c86c567b7f603f593e78e
Piraveen Mahesan <12661761+POPTJ47@users.noreply.github.com>
2024-10-16 22:51:46 -0400

    Initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Piraveen Mahesan on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
